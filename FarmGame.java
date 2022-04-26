d7a50e705037303c5b0a825c9c2c51392962280f
mjsick1 <104218664+mjsick1@users.noreply.github.com>
2022-04-26 11:51:09 -0500

    Add files via upload

--- FarmGame.java
+++ FarmGame.java
@@ -2,6 +2,9 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class FarmGame {
+
+    // Variable declarations
+
     static int money = 10000;
     static double fieldWorth;
     static int fieldStatus;
@@ -34,6 +37,9 @@ public class FarmGame {
     public static void main(String[] args) {
         SecureRandom randomNumbers = new SecureRandom();
         Scanner input = new Scanner(System.in);
+
+        // Loop that runs the game given that the conditions for the game being lost or won are not met.
+
         while (money > -10000 && money < 1000000) {
             cabbage = false;
             carrots = false;
@@ -44,8 +50,13 @@ public class FarmGame {
             spring = false;
             fall = false;
             winter = false;
+
+            // Loop that runs the shop given that the player owns no crops. If crops are owned, the season will be run.
             while (!cabbage && !carrots && !corn && !potatoes && !wheat) {
 
+
+                // SHOP \\
+                // Explains the amount of money the user has, the losing condition, and the prices for each shop item.
                 System.out.printf("You currently have $%d%n", money);
                 System.out.printf("If you have -$10000 by the end of the season, you will lose your farm!%n");
                 System.out.printf("Shop:%nYou can purchase a tractor: Poor Tractor: $%d, Decent Tractor: $%d, Great Tractor: $%d%n" +
@@ -58,11 +69,20 @@ public class FarmGame {
                         seedCost,
                         seedCost,
                         seedCost);
+
+                // Asks the user if they would like to buy a tractor
+
                 System.out.printf("%nWould you like to purchase a tractor? (y or n)%n");
+
+                // Allows the user to answer
+
                 answer = input.next();
                 if (answer.equalsIgnoreCase("y")) {
                     System.out.printf("What tractor would you like to purchase? (Poor, Decent, Great)%n");
                     answer = input.next();
+
+                    // Decides what conditions change when a poor tractor is bought
+
                     if (answer.equalsIgnoreCase("Poor")) {
                         money = money - poorTractorCost;
                         poorTractor = true;
@@ -71,6 +91,9 @@ public class FarmGame {
                         tractorString = "poor";
                         System.out.printf("You now own a Poor Tractor%n");
                         System.out.printf("You now have $%d", money);
+
+                        // Decides what conditions change when a decent tractor is bought
+
                     } else if (answer.equalsIgnoreCase("Decent")) {
                         money = money - decentTractorCost;
                         decentTractor = true;
@@ -79,6 +102,9 @@ public class FarmGame {
                         tractorString = "decent";
                         System.out.printf("You now own a Decent Tractor%n");
                         System.out.printf("You now have $%d", money);
+
+                        // Decides what conditions change when a great tractor is bought
+
                     } else if (answer.equalsIgnoreCase("Great")) {
                         money = money - greatTractorCost;
                         greatTractor = true;
@@ -87,18 +113,29 @@ public class FarmGame {
                         tractorString = "great";
                         System.out.printf("YOu now own a Great Tractor%n");
                         System.out.printf("You now have $%d", money);
+
+                        // throws an exception if an invalid input is entered
+
                     } else {
                         throw new IllegalArgumentException("Error: input must be one of the tractor types listed");
                     }
+                    // Continues the code if the user decides not to buy a tractor
                 } else if (answer.equalsIgnoreCase("n")) {
+                    // Throws an exception if an invalid input is entered.
                 } else {
                     throw new IllegalArgumentException("Error: input must be 'y' or 'n'");
                 }
+
+                // If no tractors are bought, sets the string for the constructor for SeasonConditions to represent no tractors bought.
                 if (!greatTractor && !decentTractor && !poorTractor) {
                     tractorString = "none";
                 }
+
+                // Asks the user what type of seeds they would like to purchase
                 System.out.printf("%n%nWhat seeds would you like to purchase to plant for next season?" +
                         " (cabbage, carrots, corn, potatoes, or wheat)%n");
+
+                // Allows the user to respond and adjusts conditions according to answer.
                 answer = input.next();
                 if (answer.equalsIgnoreCase("cabbage")) {
                     money = money - cabbageCost;
@@ -130,10 +167,14 @@ public class FarmGame {
                     System.out.printf("You have planted wheat%n");
                     wheat = true;
                     cropsString = "wheat";
+                    // Throws an exception if an invalid input is given
                 } else {
                     throw new IllegalArgumentException("Error: input must be one of the crops listed above");
                 }
             }
+
+            // SEASON \\
+            // Runs this section of code (the season) if a type of seed is owned
             if (cabbage | carrots | corn | potatoes | wheat) {
                 System.out.printf("The season is beginning:%n");
                 if (season > 4) {
@@ -161,10 +202,12 @@ public class FarmGame {
                         seasonString = "summer";
                         break;
                 }
+
+                // Initialization of constructor that determines the worth of a field given conditions
                 SeasonConditionsConstructor season1 = new SeasonConditionsConstructor(seasonString, tractorString, cropsString);
 
 
-
+                // Prints a field
                 for (int i = 0; i < field.length; i++) {
                     for (int j = 0; j < field[i].length; j++) {
                         System.out.printf("%d ", field[i][j]);
@@ -172,8 +215,10 @@ public class FarmGame {
                     System.out.println();
                 }
 
+                // Generates random numbers to decide what events take place in the season.
                 for(int counter = 0; counter < 25; counter++){
                     fieldStatus = randomNumbers.nextInt(25);
+                    // Switch containing what random events take place based on the fieldStatus variable
                    switch(fieldStatus){
                        case 1:
                            break;
@@ -198,26 +243,19 @@ public class FarmGame {
                            }
 
                    }
-
+                   // If fieldStatus = 1, the season is ended by breaking from the for loop
                    if (fieldStatus == 1){
-
-                       System.out.printf("Your field has grown!%n");
-                       for (int i = 0; i < field.length; i++) {
-                           for (int j = 0; j < field[i].length; j++) {
-                               System.out.printf("%d ", field[i][j]);
-                           }
-                           System.out.println();
-                       }
                        break;
                    }
 
                 }
             }
         }
-
+        // Checks the loss condition
         if (money <= -10000) {
             System.out.printf("You have lost your farm!%nGame Over!");
         }
+        // Checks the win condition
         if(money >= 1000000){
             System.out.printf("You have earned $1000000%nYou win!");
         }
